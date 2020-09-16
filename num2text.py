74bfb4ef770d1acd6931b3bd2223ff63def35cdb
Yunis Huseynzade <yunisdev.04@gmail.com>
2020-09-16 18:34:18 +0400

    quintilion support

--- num2text.py
+++ num2text.py
@@ -19,8 +19,12 @@ numsDict = {
     "90": "doxsan",
     "x": "min",
     "xx": "milyon",
-    "xxx": "milyard"
+    "xxx": "milyard",
+    "xxxx": "trilyon",
+    "xxxxx": "kvartilyon",
+    "xxxxx": "kvintilyon"
 }
+
 
 def getLadder(num):
     parts = divide_to_parts(str(num))
@@ -29,7 +33,7 @@ def getLadder(num):
         numLadder = [i for i in part]
         nums = []
         for i in range(len(numLadder)):
-            if numLadder[i]!="0":
+            if numLadder[i] != "0":
                 nums.append(numLadder[i]+"0"*(len(numLadder)-1-i))
         result.append(nums)
     return result
@@ -38,17 +42,18 @@ def getLadder(num):
 def divide_to_parts(num):
     parts = []
     left = len(num) % 3
-    if len(num[:left])>0:
+    if len(num[:left]) > 0:
         parts.append(num[:left])
     j = 0
     temp = ""
     for i in num[left:]:
         temp += i
-        if j%3==2:
+        if j % 3 == 2:
             parts.append(temp)
             temp = ""
         j += 1
     return parts
+
 
 def complect_ladder(ladder):
     result = []
@@ -56,7 +61,7 @@ def complect_ladder(ladder):
         temp = []
         for j in i:
             t = numsDict.get(j)
-            if len(j) == 3 and numsDict.get(j[0])!='bir':
+            if len(j) == 3 and numsDict.get(j[0]) != 'bir':
                 t = numsDict.get(j[0])+" yüz"
             elif len(j) == 3:
                 t = 'yüz'
